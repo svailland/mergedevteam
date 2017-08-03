cbf6ee584e1d0e14cec19bebc146e8912653c80d
Stephane Vailland <stephane.vailland@st.com>
2017-08-03 17:59:47 +0100

    merge first commit from dev3 branch in master branch

--- helloworld.c
+++ helloworld.c
@@ -6,9 +6,13 @@ int main() {
 
   printf("Hello World again, how are you ?\n");
 
+<<<<<<< HEAD
   printf("Hello World again and again, can you hear me ?\n");
 
   printf("Hello World again and again and again, do you speak english ?\n");
+=======
+  printf("Hello Paris, how are you ?\n");
+>>>>>>> bf33af8... first commit in dev3 branch
 
   return(0);
 }
